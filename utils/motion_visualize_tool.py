633b758ad0cd96a5655fda5c192153d1c3707730
kitano <kitano>
2024-10-26 20:22:30 +0900

    モーションキャプチャデータの修正と可視化

--- utils/motion_visualize_tool.py
+++ utils/motion_visualize_tool.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 from .files import FileName
-from .skeleton_keypoints import exp_keypoints_connections, exp_keypoints_list
+from .skeleton_keypoints import *
 
 
 def get_3d_motion_data_exp(
@@ -107,6 +107,398 @@ def plot_3d_motion_exp(
         frames.append(fig)
 
     vec_data = get_3d_motion_data_exp(df, min_frame)
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=vec_data[x_label],
+                y=vec_data[y_label],
+                z=vec_data[z_label],
+                mode=graph_mode,
+                line=dict(width=line_width),
+                marker=dict(size=marker_size),
+                name=name,
+            )
+            for x_label, y_label, z_label, name in zip(
+                x_vec_label, y_vec_label, z_vec_label, vec_name
+            )
+        ],
+        frames=frames,
+    )
+
+    steps = []
+    for frame in range(min_frame, max_frame + 1, frame_step):
+        step = dict(
+            method="animate",
+            args=[
+                [f"{frame}"],
+                dict(frame=dict(duration=1, redraw=True), mode="immediate"),
+            ],
+            label=f"{frame}",
+        )
+        steps.append(step)
+
+    sliders = [
+        dict(
+            steps=steps,
+            active=0,
+            transition=dict(duration=0),
+            currentvalue=dict(
+                font=dict(size=20), prefix="", visible=True, xanchor="right"
+            ),
+        )
+    ]
+
+    fig.update_layout(
+        scene=dict(
+            camera=dict(eye=dict(x=x_max, y=y_max, z=z_max)),
+            xaxis=dict(title="X [m]", range=[x_min, x_max]),
+            yaxis=dict(title="Y [m]", range=[y_min, y_max]),
+            zaxis=dict(title="Z [m]", range=[z_min, z_max]),
+            aspectmode="manual",
+            aspectratio=dict(
+                x=(x_max - x_min), y=(y_max - y_min), z=(z_max - z_min)
+            ),
+        ),
+        updatemenus=[
+            dict(
+                type="buttons",
+                showactive=False,
+                xanchor="left",
+                yanchor="top",
+                x=0,
+                y=1,
+                buttons=[
+                    dict(
+                        label="Play",
+                        method="animate",
+                        args=[
+                            None,
+                            dict(
+                                frame=dict(duration=1, redraw=True),
+                                fromcurrent=True,
+                                transition=dict(duration=0),
+                            ),
+                        ],
+                    ),
+                    dict(
+                        label="Pause",
+                        method="animate",
+                        args=[
+                            [None],
+                            dict(
+                                frame=dict(duration=0, redraw=False),
+                                mode="immediate",
+                                transition=dict(duration=0),
+                            ),
+                        ],
+                    ),
+                ],
+            )
+        ],
+        sliders=sliders,
+    )
+
+    if graph_show:
+        fig.update_layout(height=800, width=1200)
+        fig.show()
+    else:
+        fig.write_html(output_path, auto_play=False)
+
+
+def get_3d_motion_data_trajectory(
+    df: pd.DataFrame,
+    frame: int,
+) -> dict:
+    target_dict = {}
+    for part, (start, end) in keypoints_connections.items():
+        start = keypoints_list[start]
+        end = keypoints_list[end]
+        for axis in ["x", "y", "z"]:
+            key = f"{part}_{axis}"
+            target_dict[key] = df[[f"{start}_{axis}", f"{end}_{axis}"]].loc[
+                frame
+            ]
+    return target_dict
+
+
+def plot_3d_motion_trajectory(
+    threed_data: Union[pd.DataFrame, str],
+    line_width: int = 10,
+    marker_size: int = 3,
+    graph_mode: str = "lines+markers",
+    frame_step: int = 1,
+    output_name: str = None,
+    point_show: bool = False,
+    club_show: bool = False,
+    graph_show: bool = False,
+) -> None:
+    if output_name is None:
+        output_name = FileName.threed_motion
+    else:
+        if not output_name.endswith(".html"):
+            output_name += ".html"
+    # Read the 3D motion data
+    if isinstance(threed_data, pd.DataFrame):
+        df = threed_data
+        output_path = os.path.join(os.getcwd(), output_name)
+    else:
+        data_dir = os.path.dirname(threed_data)
+        output_path = os.path.join(data_dir, output_name)
+
+        df = pd.read_csv(threed_data)
+        df.ffill(inplace=True)
+        df = df.loc[(df.filter(like="_x").sum(axis=1) != 0)]
+
+    if point_show:
+        point_columns = [
+            col.replace("_x", "")
+            for col in df.columns
+            if col.endswith("_x") and col.startswith("POINT")
+        ]
+        point_combinations = list(combinations(point_columns, 2))
+        for i, (start, end) in enumerate(point_combinations):
+            keypoints_connections[f"LINE{i+1}"] = [start, end]
+
+    if club_show:
+        keypoints_connections["SHAFT"] = ["GRIP", "HOSEL"]
+        keypoints_connections["HEAD"] = ["HOSEL", "TOE"]
+
+    # Create the 3D motion plot
+    x_max = df.filter(like="_x").max().max()
+    x_min = df.filter(like="_x").min().min()
+    y_max = df.filter(like="_y").max().max()
+    y_min = df.filter(like="_y").min().min()
+    z_max = df.filter(like="_z").max().max()
+    z_min = df.filter(like="_z").min().min()
+    min_frame = df.index.min()
+    max_frame = df.index.max()
+
+    frames = []
+    for frame in range(min_frame, max_frame + 1, frame_step):
+        vec_data = get_3d_motion_data_trajectory(df, frame)
+
+        x_vec_label = list(vec_data.keys())[0::3]
+        y_vec_label = list(vec_data.keys())[1::3]
+        z_vec_label = list(vec_data.keys())[2::3]
+        vec_name = [label.replace("_x", "") for label in x_vec_label]
+
+        fig = go.Frame(
+            data=[
+                go.Scatter3d(
+                    x=vec_data[x_label],
+                    y=vec_data[y_label],
+                    z=vec_data[z_label],
+                    mode=graph_mode,
+                    line=dict(width=line_width),
+                    marker=dict(size=marker_size),
+                    name=name,
+                )
+                for x_label, y_label, z_label, name in zip(
+                    x_vec_label, y_vec_label, z_vec_label, vec_name
+                )
+            ],
+            name=f"{frame}",
+            layout=go.Layout(title=f"frame:{frame}"),
+        )
+        frames.append(fig)
+
+    vec_data = get_3d_motion_data_trajectory(df, min_frame)
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=vec_data[x_label],
+                y=vec_data[y_label],
+                z=vec_data[z_label],
+                mode=graph_mode,
+                line=dict(width=line_width),
+                marker=dict(size=marker_size),
+                name=name,
+            )
+            for x_label, y_label, z_label, name in zip(
+                x_vec_label, y_vec_label, z_vec_label, vec_name
+            )
+        ],
+        frames=frames,
+    )
+
+    steps = []
+    for frame in range(min_frame, max_frame + 1, frame_step):
+        step = dict(
+            method="animate",
+            args=[
+                [f"{frame}"],
+                dict(frame=dict(duration=1, redraw=True), mode="immediate"),
+            ],
+            label=f"{frame}",
+        )
+        steps.append(step)
+
+    sliders = [
+        dict(
+            steps=steps,
+            active=0,
+            transition=dict(duration=0),
+            currentvalue=dict(
+                font=dict(size=20), prefix="", visible=True, xanchor="right"
+            ),
+        )
+    ]
+
+    fig.update_layout(
+        scene=dict(
+            camera=dict(eye=dict(x=x_max, y=y_max, z=z_max)),
+            xaxis=dict(title="X [m]", range=[x_min, x_max]),
+            yaxis=dict(title="Y [m]", range=[y_min, y_max]),
+            zaxis=dict(title="Z [m]", range=[z_min, z_max]),
+            aspectmode="manual",
+            aspectratio=dict(
+                x=(x_max - x_min), y=(y_max - y_min), z=(z_max - z_min)
+            ),
+        ),
+        updatemenus=[
+            dict(
+                type="buttons",
+                showactive=False,
+                xanchor="left",
+                yanchor="top",
+                x=0,
+                y=1,
+                buttons=[
+                    dict(
+                        label="Play",
+                        method="animate",
+                        args=[
+                            None,
+                            dict(
+                                frame=dict(duration=1, redraw=True),
+                                fromcurrent=True,
+                                transition=dict(duration=0),
+                            ),
+                        ],
+                    ),
+                    dict(
+                        label="Pause",
+                        method="animate",
+                        args=[
+                            [None],
+                            dict(
+                                frame=dict(duration=0, redraw=False),
+                                mode="immediate",
+                                transition=dict(duration=0),
+                            ),
+                        ],
+                    ),
+                ],
+            )
+        ],
+        sliders=sliders,
+    )
+
+    if graph_show:
+        fig.update_layout(height=800, width=1200)
+        fig.show()
+    else:
+        fig.write_html(output_path, auto_play=False)
+
+
+def get_3d_motion_data_body(
+    df: pd.DataFrame,
+    frame: int,
+) -> dict:
+    target_dict = {}
+    for part, (start, end) in body_keypoints_connections.items():
+        start = exp_keypoints_list[start]
+        end = exp_keypoints_list[end]
+        for axis in ["x", "y", "z"]:
+            key = f"{part}_{axis}"
+            target_dict[key] = df[[f"{start}_{axis}", f"{end}_{axis}"]].loc[
+                frame
+            ]
+    return target_dict
+
+
+def plot_3d_motion_body(
+    threed_data: Union[pd.DataFrame, str],
+    line_width: int = 10,
+    marker_size: int = 3,
+    graph_mode: str = "lines+markers",
+    frame_step: int = 1,
+    output_name: str = None,
+    point_show: bool = False,
+    club_show: bool = False,
+    graph_show: bool = False,
+) -> None:
+    if output_name is None:
+        output_name = FileName.threed_motion
+    else:
+        if not output_name.endswith(".html"):
+            output_name += ".html"
+    # Read the 3D motion data
+    if isinstance(threed_data, pd.DataFrame):
+        df = threed_data
+        output_path = os.path.join(os.getcwd(), output_name)
+    else:
+        data_dir = os.path.dirname(threed_data)
+        output_path = os.path.join(data_dir, output_name)
+
+        df = pd.read_csv(threed_data)
+        df.ffill(inplace=True)
+        df = df.loc[(df.filter(like="_x").sum(axis=1) != 0)]
+
+    if point_show:
+        point_columns = [
+            col.replace("_x", "")
+            for col in df.columns
+            if col.endswith("_x") and col.startswith("POINT")
+        ]
+        point_combinations = list(combinations(point_columns, 2))
+        for i, (start, end) in enumerate(point_combinations):
+            keypoints_connections[f"LINE{i+1}"] = [start, end]
+
+    if club_show:
+        keypoints_connections["SHAFT"] = ["GRIP", "HOSEL"]
+        keypoints_connections["HEAD"] = ["HOSEL", "TOE"]
+
+    # Create the 3D motion plot
+    x_max = df.filter(like="_x").max().max()
+    x_min = df.filter(like="_x").min().min()
+    y_max = df.filter(like="_y").max().max()
+    y_min = df.filter(like="_y").min().min()
+    z_max = df.filter(like="_z").max().max()
+    z_min = df.filter(like="_z").min().min()
+    min_frame = df.index.min()
+    max_frame = df.index.max()
+
+    frames = []
+    for frame in range(min_frame, max_frame + 1, frame_step):
+        vec_data = get_3d_motion_data_body(df, frame)
+
+        x_vec_label = list(vec_data.keys())[0::3]
+        y_vec_label = list(vec_data.keys())[1::3]
+        z_vec_label = list(vec_data.keys())[2::3]
+        vec_name = [label.replace("_x", "") for label in x_vec_label]
+
+        fig = go.Frame(
+            data=[
+                go.Scatter3d(
+                    x=vec_data[x_label],
+                    y=vec_data[y_label],
+                    z=vec_data[z_label],
+                    mode=graph_mode,
+                    line=dict(width=line_width),
+                    marker=dict(size=marker_size),
+                    name=name,
+                )
+                for x_label, y_label, z_label, name in zip(
+                    x_vec_label, y_vec_label, z_vec_label, vec_name
+                )
+            ],
+            name=f"{frame}",
+            layout=go.Layout(title=f"frame:{frame}"),
+        )
+        frames.append(fig)
+
+    vec_data = get_3d_motion_data_body(df, min_frame)
     fig = go.Figure(
         data=[
             go.Scatter3d(
--- utils/skeleton_keypoints.py
+++ utils/skeleton_keypoints.py
@@ -25,6 +25,49 @@ keypoints_list = [
     'LEAR'
 ]
 
+keypoints_connections = {
+    "FRONT_SHOULDER": [10, 4],           # LSHF, RSHF
+    "BACK_SHOULDER": [11, 5],            # LSHB, RSHB
+    "RIGHT_SHOULDER": [4, 5],            # RSHF, RSHB
+    "LEFT_SHOULDER": [10, 11],           # LSHF, LSHB
+    "LEFT_UPPER_ARM_OUT": [11, 8],       # LSHB, LELL
+    "LEFT_UPPER_ARM_IN": [10, 9],        # LSHF, LELM
+    "RIGHT_UPPER_ARM_OUT": [5, 2],       # RSHB, RELL
+    "RIGHT_UPPER_ARM_IN": [4, 3],        # RSHF, RELM
+    "LEFT_FOREARM_IN": [9, 7],           # LELM, LWRM
+    "LEFT_FOREARM_OUT": [8, 6],          # LELL, LWRL
+    "RIGHT_WRIST": [1, 0],               # RWRM, RWRL
+    "LEFT_WRIST": [7, 6],                # LWRM, LWRL
+    "RIGHT_FOREARM_IN": [3, 1],          # RELM, RWRM
+    "RIGHT_FOREARM_OUT": [2, 0],         # RELL, RWRL
+    "RIGHT_ELBOW": [3, 2],               # RELM, RELL
+    "LEFT_ELBOW": [9, 8],                # LELM, LELL
+    "LEFT_HIP": [23, 21],                # LEAR, LTRO
+    "FRONT_HIP": [21, 16],               # LTRO, RTRO
+    "BACK_HIP": [23, 22],                # LEAR, REAR
+    "RIGHT_HIP": [22, 16],               # REAR, RTRO
+    "RIGHT_KNEE_OUT": [22, 14],          # REAR, RKNL
+    "RIGHT_KNEE_IN": [22, 15],           # REAR, RKNM
+    "LEFT_KNEE_OUT": [23, 19],           # LEAR, LKNL
+    "LEFT_KNEE_IN": [23, 20],            # LEAR, LKNM
+    "LEFT_BODY_FRONT": [10, 21],         # LSHF, LTRO
+    "RIGHT_BODY_FRONT": [4, 16],         # RSHF, RTRO
+    "LEFT_BODY_BACK": [11, 23],          # LSHB, LEAR
+    "RIGHT_BODY_BACK": [5, 22],          # RSHB, REAR
+    "LEFT_KNEE": [20, 19],               # LKNM, LKNL
+    "RIGHT_KNEE": [15, 14],              # RKNM, RKNL
+    "LEFT_THIGH_OUT": [21, 19],          # LTRO, LKNL
+    "RIGHT_THIGH_OUT": [16, 14],         # RTRO, RKNL
+    "LEFT_THIGH_IN": [21, 20],           # LTRO, LKNM
+    "RIGHT_THIGH_IN": [16, 15],          # RTRO, RKNM
+    "RIGHT_LOWER_LEG_OUT": [14, 12],     # RKNL, RANL
+    "LEFT_LOWER_LEG_OUT": [19, 17],      # LKNL, LANL
+    "LEFT_LOWER_LEG_IN": [20, 18],       # LKNM, LANM
+    "RIGHT_LOWER_LEG_IN": [15, 13],      # RKNM, RANM
+    "RIGHT_ANKLE": [13, 12],             # RANM, RANL
+    "LEFT_ANKLE": [18, 17]               # LANM, LANL
+}
+
 exp_keypoints_list = [
     "NOSE",
     "LEFT_EYE",
@@ -50,6 +93,51 @@ exp_keypoints_connections = {
     "NOSE_RIGHT_EYE": [0, 2],
     "LEFT_EYE_EAR": [1, 3],
     "RIGHT_EYE_EAR": [2, 4],
+    "SHOULDER": [5, 6],
+    "LEFT_UPPER_ARM": [5, 7],
+    "RIGHT_UPPER_ARM": [6, 8],
+    "LEFT_FORE_ARM": [7, 9],
+    "RIGHT_FORE_ARM": [8, 10],
+    "LEFT_TORSO": [5, 11],
+    "RIGHT_TORSO": [6, 12],
+    "GROIN": [11, 12],
+    "LEFT_THIGH": [11, 13],
+    "RIGHT_THIGH": [12, 14],
+    "LEFT_LOWER_LEG": [13, 15],
+    "RIGHT_LOWER_LEG": [14, 16],
+}
+
+keypoint_pairs_for_calc = {
+    "LEFT_SHOULDER": ["LSHF", "LSHB"],
+    "RIGHT_SHOULDER": ["RSHF", "RSHB"],
+    "LEFT_ELBOW": ["LELM", "LELL"],
+    "RIGHT_ELBOW": ["RELM", "RELL"],
+    "LEFT_WRIST": ["LWRM", "LWRL"],
+    "RIGHT_WRIST": ["RWRM", "RWRL"],
+    "LEFT_HIP": ["LEAR", "LTRO"],
+    "RIGHT_HIP": ["REAR", "RTRO"],
+    "LEFT_KNEE": ["LKNM", "LKNL"],
+    "RIGHT_KNEE": ["RKNM", "RKNL"],
+    "LEFT_ANKLE": ["LANM", "LANL"],
+    "RIGHT_ANKLE": ["RANM", "RANL"]
+}
+
+body_keypoints_list = [
+    "LEFT_SHOULDER",
+    "RIGHT_SHOULDER",
+    "LEFT_ELBOW",
+    "RIGHT_ELBOW",
+    "LEFT_WRIST",
+    "RIGHT_WRIST",
+    "LEFT_HIP",
+    "RIGHT_HIP",
+    "LEFT_KNEE",
+    "RIGHT_KNEE",
+    "LEFT_ANKLE",
+    "RIGHT_ANKLE"
+]
+
+body_keypoints_connections = {
     "SHOULDER": [5, 6],
     "LEFT_UPPER_ARM": [5, 7],
     "RIGHT_UPPER_ARM": [6, 8],
